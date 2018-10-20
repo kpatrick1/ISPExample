adbd51474704fbe1476d2113f36a6f0c1a12a674
Kevin <kpatrick@my.wctc.edu>
2018-10-20 14:04:11 -0500

    Initial commit

---
+++ src/isp/lab1/Workable.java
@@ -0,0 +1,17 @@
+package isp.lab1;
+
+public interface Workable {
+
+    public void talk(String s);
+    public void run(int distance);
+
+    public void jump();
+    public void hike();
+    public void swim();
+    public void trip();
+
+    public void start();
+    public void drive(int distance);
+    public void stopByBreaking();
+
+}
